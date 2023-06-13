b87108ba1cb3592989d1360bf3160b009fe75903
marlenovtilek <marlenovtilek@gmail.com>
2023-06-13 23:16:32 +0600

    First initial commit

---
+++ cars/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+from django.core.validators import MaxValueValidator
+
+# Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField("Марка автомобиля", max_length=50)
+    model = models.CharField("Модель автомобиля", max_length=50)
+    license_plate = models.CharField("Государственный номер автомобиля", max_length=10, null=True, blank=True)
+    vehicle_type = models.CharField("Тип кузова", max_length=20)
+    image = models.ImageField("Фотография автомобиля", upload_to="cars/images", null=True)
+    year_production = models.IntegerField("Год выпуска", null=True, validators=[MaxValueValidator(2050)])
+    color = models.CharField("Цвет", max_length=20, null=True)
+
+
+    class Meta:
+        verbose_name = "Автомобиль"
+        verbose_name_plural = "Автомобили"
+
+    def __str__(self):
+        return self.brand
+    
