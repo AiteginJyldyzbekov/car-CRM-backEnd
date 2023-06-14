70d2e5f57ecdce59f505ab5d6c2bd4d12e7197f1
marlenovtilek <marlenovtilek@gmail.com>
2023-06-15 02:47:55 +0600

    Added filters, search and to_representation

---
+++ cars/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as rest_filters, CharFilter
+
+from cars.models import Car
+
+class CarFilter(rest_filters.FilterSet):
+    brand = CharFilter(field_name='brand', lookup_expr='icontains')
+    model = CharFilter(field_name='model', lookup_expr='icontains')
+    vehicle_type = CharFilter(field_name='vehicle_type', lookup_expr='icontains')
+
+
+    class Meta:
+        model = Car
+        fields = ['brand','model','vehicle_type']
