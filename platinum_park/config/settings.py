8f053eb2ffa31aa1561409e13d99db424914b756
internetwarrior <makarov.kake312@gmail.com>
2023-07-12 19:02:41 +0600

    Commit

--- platinum_park/config/settings.py
+++ platinum_park/config/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/4.2/ref/settings/
+https://do cs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from datetime import timedelta
